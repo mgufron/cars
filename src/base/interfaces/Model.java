a1c2dfb5c21d8aa5a81c64518c2b23c231609621
Mochamad Gufron <mgufronefendi@gmail.com>
2013-07-11 12:51:34 +0700

    now

--- src/base/interfaces/Model.java
+++ src/base/interfaces/Model.java
@@ -13,19 +13,7 @@ import java.util.Map;
  */
 public interface Model {
 //	Make where conditional statement before creating query
-	public Model where(String where);
-//	Make order statement before creating query
-	public Model order(String order);
-//	if it is new record
-	public boolean save();
-//	Saving new record
-	public boolean insert();
-//	updating current record
-	public boolean update();
-	
+	public int countAll();
 	public ArrayList<Map<String, String>> findAll();
 	
-	public Map<String, String> find();
-	
-	public Model setAttributes(Map<String, String> attributes);
 }
